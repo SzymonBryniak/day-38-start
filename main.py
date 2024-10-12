11dcd75d1fff68154d663657a6c975d6abcdbad0
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-10-12 15:17:58 +0100

    day 38 lesson 288 exercise completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 
 os.environ["APP_ID"] = "b0fcd0ea"
 os.environ["API_KEY"] = "f5b51cc048a7e294674a7bb9a5b33dc2"
@@ -8,11 +9,16 @@ APP_ID = os.environ.get("APP_ID")
 API_KEY = os.environ.get("API_KEY")
 
 header = {
+    'Content-Type': 'application/json',
     "x-app-id": APP_ID,
-    "x=app-key": API_KEY
+    "x-app-key": API_KEY
 }
 
-exercise_stats = "Ran 2 miles and walked for 3km."
-response = requests.post(url="https://v2/natural/exercise", data=exercise_stats, json="json", headers=header)
+body = {
+    "query": input("Tell me which exercises you did: ")
+}
+
+
+response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", json=body, headers=header)
 response.raise_for_status()
 print(response.text)
